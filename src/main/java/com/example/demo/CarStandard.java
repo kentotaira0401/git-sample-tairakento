5c6030dc80d42c026ef86408ca354ca9c7c74159
tairakento <kento.taira@rakus.co.jp>
2018-10-30 11:43:25 +0900

    GitStandard新規追加

---
+++ src/main/java/com/example/demo/CarStandard.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+public class CarStandard {
+		
+	private String Color;
+
+	public String getColor() {
+		return Color;
+	}
+
+	public void setColor(String color) {
+		Color = color;
+	}
+	
+}
