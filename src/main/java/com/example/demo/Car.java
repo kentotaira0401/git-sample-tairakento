be51f074d7764dd0b4e316f3918f8eb176cc6e35
tairakento <kento.taira@rakus.co.jp>
2018-10-30 11:25:59 +0900

    新規にCarクラスを作成　及び　スタートプログラムにコメントをすつりょくするように修正

---
+++ src/main/java/com/example/demo/Car.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+/*
+ * 
+ * 車を表すドメイン
+ * @author taira
+ * 
+ */
+public class Car {
+
+	private Integer speed;
+
+	public Integer getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(Integer speed) {
+		this.speed = speed;
+	}
+	
+}
